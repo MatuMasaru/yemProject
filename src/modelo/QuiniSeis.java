08e6f601267c2411730dd9126587856aaa70a741
Eddy <eavg_93@outlook.com>
2020-02-04 23:39:09 -0300

    La modifica la clase Casilla QuiniSeis en el método de acción.
    Se modifica su método al llamar el Jugador a su método específico que realiza el cambio de estado según corresponde.
    La idea de la secuencia es que el jugador sabe según su estado en qué visita está sin tener que estar preguntando y sin un contador.

--- src/modelo/QuiniSeis.java
+++ src/modelo/QuiniSeis.java
@@ -1,13 +1,12 @@
 package modelo;
 
-import static modelo.Constantes.*;
-
 public class QuiniSeis extends Casilla {
 
     @Override
     public void accion(Jugador unJugador) {
 
-        unJugador.incrementoCapital(PREMIO_INICIAL_QUINISEIS);
+        unJugador.visitaConPremio();
 
     }
+
 }
