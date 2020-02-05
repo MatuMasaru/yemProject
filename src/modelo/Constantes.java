586b62157d758c16bf66a75ea1e143dc589fa580
Eddy <eavg_93@outlook.com>
2020-02-04 22:06:32 -0300

    Arreglo de la constante PREMIO_INICIAL_QUINISEIS.
    Se arregla el premio de 5000 a 50000.

--- src/modelo/Constantes.java
+++ src/modelo/Constantes.java
@@ -4,6 +4,6 @@ public class Constantes {
 
     static public Integer CAPITAL_INICIAL_JUGADOR = 100000;
 
-    static public Integer PREMIO_INICIAL_QUINISEIS = 5000;
+    static public Integer PREMIO_INICIAL_QUINISEIS = 50000;
 
 }
