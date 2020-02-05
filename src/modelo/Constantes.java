393b0140d05f2392f442cb1f649ece060c24cbaa
Eddy <eavg_93@outlook.com>
2020-02-04 23:27:49 -0300

    Se agrega nuevas constantes estáticas: SEGUNDO_PREMIO_QUINISEIS, SIGUIENTES_PREMIOS_QUINISEIS.

--- src/modelo/Constantes.java
+++ src/modelo/Constantes.java
@@ -4,6 +4,8 @@ public class Constantes {
 
     static public Integer CAPITAL_INICIAL_JUGADOR = 100000;
 
-    static public Integer PREMIO_INICIAL_QUINISEIS = 50000;
+    static public Integer PRIMER_PREMIO_QUINISEIS = 50000;
+    static public Integer SEGUNDO_PREMIO_QUINISEIS = 30000;
+    static public Integer SIGUIENTES_PREMIOS_QUINISEIS = 0;
 
 }
