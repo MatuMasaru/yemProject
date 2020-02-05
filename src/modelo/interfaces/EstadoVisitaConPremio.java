4d65f5cfb1534c3f96bb3ed2b265b8a2f3b28b13
Eddy <eavg_93@outlook.com>
2020-02-05 00:01:05 -0300

    Se crea el estado donde realizaremos el patron State para Jugador y su visita a la casilla Quini6.
    Con este patrón se arreglan el estar realizando un contador cada que pasa cierta cantidad de veces por Quini6.

---
+++ src/modelo/interfaces/EstadoVisitaConPremio.java
@@ -0,0 +1,11 @@
+package modelo.interfaces;
+
+import modelo.Jugador;
+
+public interface EstadoVisitaConPremio {
+
+    void nuevaVisita(Jugador unJugador);
+
+    void premioDeVisita(Jugador unJugador);
+
+}
